9dfb130adaa665123f54cca6c309406d37569473
Joel Jagarath <joey.jagarath05@gmail.com>
2019-09-23 21:10:54 +0200

    Added radio buttons for different User Types

--- Limit Breakers/Models/Register.cs
+++ Limit Breakers/Models/Register.cs
@@ -13,11 +13,17 @@ namespace Limit_Breakers.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserID { get; set; }
+        [DisplayName("Email Address:")]
         public string Email { get; set; }
+        [DisplayName("Username:")]
         public string Username { get; set; }
+        [DisplayName("Password:")]
         public string Password { get; set; }
+        [DisplayName("Tokens:")]
         public int Tokens { get; set; }
+        [DisplayName("Phone Number:")]
         public string PhoneNumber { get; set; }
+        [DisplayName("User Type:")]
         public string UserType { get; set; }
 
     }
